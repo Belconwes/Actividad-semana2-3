b748a9c55ac2e4e968cc96335f483c464338f341
bel <gaunaabeltiago@gmail.com>
2024-04-26 19:45:31 -0300

    dwd

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -27,6 +27,12 @@ namespace MvcMovie.Controllers
             return View();
         }
 
+        public IActionResult West()
+        {
+            return View();
+
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
